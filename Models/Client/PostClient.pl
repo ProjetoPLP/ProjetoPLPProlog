1d3bda2b1f41bbbb72f43b6e3595c451875e8325
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2023-10-08 09:53:12 -0300

    Add Assets Feito

---
+++ Models/Client/PostClient.pl
@@ -0,0 +1,41 @@
+:- consult('GetSetAttrsClient.pl').
+:- consult('SaveClient.pl').
+:- consult('ModelClient.pl').
+
+addAsset(ClientID, CompanyID, Qtd, Result) :- 
+    getClient(ClientID, Client),
+    Client = client(Ident, Name, Age, Cpf, Email, Password, Cash, Patrimony, CanDeposit, Row, Col, AllAssets),
+    exam(AllAssets, CompanyID, Qtd, NewAllAssets),
+    deleteZeroOrNegative(NewAllAssets, NewAllAssetsPositive),
+    NewClient = client(Ident, Name, Age, Cpf, Email, Password, Cash, Patrimony, CanDeposit, Row, Col, NewAllAssetsPositive),
+    editClientJSON(NewClient),
+    Result = true.
+
+exam(AllAssets, CompanyID, Qtd, NewAllAssets) :-
+    buscarEmpresa(AllAssets, CompanyID, OldQtd),
+    NovoQtd is OldQtd + Qtd,
+    atualizarEmpresa(AllAssets, CompanyID, NovoQtd, NewAllAssets).
+
+atualizarEmpresa([], CompanyID, Qtd, [[CompanyID, Qtd]]).
+atualizarEmpresa([[CompanyID, _]|Rest], CompanyID, NovoQtd, [[CompanyID, NovoQtd]|Rest]).
+atualizarEmpresa([X|Rest], CompanyID, Qtd, [X|NewRest]) :-
+    atualizarEmpresa(Rest, CompanyID, Qtd, NewRest).
+
+buscarEmpresa([], _, 0).
+buscarEmpresa([[CompanyID, Qtd]|_], CompanyID, Qtd).
+buscarEmpresa([_|Rest], CompanyID, Qtd) :-
+    buscarEmpresa(Rest, CompanyID, Qtd).
+
+deleteZeroOrNegative([], []).
+deleteZeroOrNegative([[CompanyID, Qtd]|Rest], Filtered) :-
+    (Qtd > 0 ->
+        Filtered = [[CompanyID, Qtd]|NewRest]
+    ;
+        Filtered = NewRest
+    ),
+    deleteZeroOrNegative(Rest, NewRest).
+
+main :- 
+    addAsset(1, 10, -110, Result), 
+    write(Result),
+    halt.
