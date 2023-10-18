a6290c2538029d275b5d7b54e5dcaef796998feb
Daniel Mantovani <daniel.mantovani047@gmail.com>
2023-10-17 21:19:33 -0300

    Fix verificationUtils

--- Utils/verificationUtils.pl
+++ Utils/verificationUtils.pl
@@ -2,14 +2,14 @@
 
 
 % Verifica se existe uma empresa cadastrada a bolsa a partir do seu ID
-existCompany(Id, R) :-
-    getCompanyJSON(Comps),
-    existCompanyAux(Id, Comps, R).
+existCompany(JSONPath, Id, R) :-
+    getCompanyJSON(JSONPath, Comps),
+    existCompanyAux(JSONPath, Id, Comps, R).
 
-existCompanyAux(_, [], false) :- !.
-existCompanyAux(Id, (H|T), R) :-
-    getIdent(H, CompId),
-    (CompId =:= Id -> true ; existCompanyAux(Id, T, R)).
+existCompanyAux(_, _, [], false) :- !.
+existCompanyAux(JSONPath, Id, (H|T), R) :-
+    getIdent(JSONPath, H, CompId),
+    (CompId =:= Id -> true ; existCompanyAux(JSONPath, Id, T, R)).
 
 
 % Verifica se uma String é um número
