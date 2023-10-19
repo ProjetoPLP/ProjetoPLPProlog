6246654fedfe938d5d78cb60784489878bd2d1cb
Daniel Mantovani <daniel.mantovani047@gmail.com>
2023-10-18 21:26:57 -0300

    Fix bug diretório Clock.json

--- Models/Clock/GetSetClock.pl
+++ Models/Clock/GetSetClock.pl
@@ -5,24 +5,24 @@ lerJSON(JSONPath, File) :-
 	open(JSONPath, read, F),
 	json_read_dict(F, File).
 
-getClock(JSONPath, Minutes) :- 
-    open(JSONPath, read, F),
+getClock(Minutes) :- 
+    open("../Data/Clock.json", read, F),
     json_read_dict(F, Clock),
     Minutes is Clock.minutes.
 
-saveClock(JSONPath, Minutes) :- 
+saveClock(Minutes) :- 
     clockToJSON(Minutes, Saida),
-    open(JSONPath, write, Stream),
+    open("../Data/Clock.json", write, Stream),
     write(Stream, Saida),
     close(Stream).
 
 clockToJSON(Minutes, Out) :- 
     swritef(Out, '{"minutes": %w}', [Minutes]).
 
-addClock(JSONPath, Value) :- 
-    getClock(JSONPath, MinutesAtual),
+addClock(Value) :- 
+    getClock(MinutesAtual),
     NovoTempo is MinutesAtual + Value,
-    saveClock(JSONPath, NovoTempo).
+    saveClock(NovoTempo).
 
-setClock(JSONPath, Value) :- 
-    saveClock(JSONPath, Value).
+setClock(Value) :- 
+    saveClock(Value).
