c8a1cc83b5270ef89bc9f3eb91603c0e2b9783e9
Daniel Mantovani <daniel.mantovani047@gmail.com>
2023-10-19 23:23:23 -0300

    Fix HomeBrokerLoopLogic

--- Menus/HomeBroker/HomeBrokerLoopLogic.pl
+++ Menus/HomeBroker/HomeBrokerLoopLogic.pl
@@ -1,27 +1,24 @@
-:- use_module(library(time)).
-:- use_module(library(timeout)).
-:- use_module(library(random)).
 :- consult('../../Utils/GraphUtilsHomeBroker.pl').
 :- consult('../../Utils/GraphUtilsWallet.pl').
-:- consult('HomeBrokerAttPrice.pl').
+:- consult('./HomeBrokerAttPrice.pl').
 :- consult('../Wallet/WalletAttPatrimony.pl').
-:- consult('CompanyDown/CompanyDownUpdate.pl').
+:- consult('./CompanyDown/CompanyDownUpdate.pl').
 :- consult('../../Models/Company/SaveCompany.pl').
 :- consult('../../Models/Client/SaveClient.pl').
 :- consult('../../Models/Clock/GetSetClock.pl').
 
 
+% Define, a partir da entrada do usuário, por quanto tempo o preço e o gráfico deve variar
 callLoop(IdComp, Seconds) :-
     get_time(StartTime),
     addClock(Seconds),
-    number_string(IdComp, IdString),
-    string_concat("./Models/Company/HomeBrokers/homebroker", IdString, Path0),
-    string_concat(Path0, ".txt", Path),
-    updateMatrixClock(Path),
-    add_seconds_to_time(StartTime, Seconds, EndTime),
+    homeBrokerFilePath(IdComp, FilePath),
+    updateMatrixClock(FilePath),
+    EndTime is StartTime + Seconds,
     loop(IdComp, EndTime).
 
 
+% Atualiza o gráfico nas empresas e carteiras
 loop(IdComp, EndTime) :-
     get_time(CurrentTime),
     (   (CurrentTime >= EndTime ; isDown(IdComp))
@@ -34,13 +31,6 @@ loop(IdComp, EndTime) :-
         getCompanyJSON(Companies),
         attAllCompanyPriceGraph(IdComp, Companies),
         attAllClientsPatrimonyGraph(Clients),
-        sleep(0.5),  % Aguarda meio segundo (500 milissegundos)
+        sleep(0.5),
         loop(IdComp, EndTime)
     ).
-
-
-add_seconds_to_time(StartTime, Seconds, EndTime) :-
-    call_with_timeout(add_seconds(StartTime, Seconds), EndTime, 1000). % Timeout set to 1000 ms.
-
-add_seconds(StartTime, Seconds, EndTime) :-
-    EndTime is StartTime + Seconds.
